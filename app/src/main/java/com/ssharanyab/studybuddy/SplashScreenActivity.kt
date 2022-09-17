e56a49b9280360de3a559c0c20c9691e58419ada
ssharanyab <ssharanyab@gmail.com>
2022-09-17 19:47:08 +0530

    progress bar updated

--- app/src/main/java/com/ssharanyab/studybuddy/SplashScreenActivity.kt
+++ app/src/main/java/com/ssharanyab/studybuddy/SplashScreenActivity.kt
@@ -1,11 +1,30 @@
 package com.ssharanyab.studybuddy
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val handler: Handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+                            },3000)
+
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.max =1000
+        val currentProgress  =1000
+        ObjectAnimator.ofInt(progressBar,"progress",currentProgress).setDuration(3000).start()
+
+
+
     }
 }
